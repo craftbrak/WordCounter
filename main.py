d3629b1405379fcfdc3dadedd79d60c948510bb0
craftbrak <louisdewilde2001@gmail.com>
2023-04-24 21:39:38 +0200

    adding a full score table and fixing timer

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class WordCounterApp:
         self.misshit_count = 0
         self.time_left = 0
         self.running = False
+        self.scores = []
 
         self.label = tk.Label(self.root, text="Words per Minute: 0\nMisshit Count: 0\nTime Left: 0", font=("Arial", 18),
                               justify=tk.LEFT)
@@ -23,9 +24,13 @@ class WordCounterApp:
         self.reset_button = tk.Button(self.root, text="Reset Counter", command=self.reset_counter)
         self.reset_button.pack(side=tk.RIGHT, padx=10)
 
+        self.score_table = tk.Label(self.root, text="Score Table:", font=("Arial", 14), justify=tk.LEFT)
+        self.score_table.pack(anchor='center', pady=10)
+
         self.monitor_key_presses()
 
     def start_countdown(self):
+        self.reset_counter()
         self.start_button.config(state=tk.DISABLED)
         self.reset_button.config(state=tk.DISABLED)
         self.label.config(text="Starting in 4 seconds...")
@@ -37,36 +42,49 @@ class WordCounterApp:
     def start_timer(self):
         self.running = True
         self.time_left = 60
-        self.update_label()
+        self.update_score_label()
+        self.update_time_label()
         self.root.after(60000, self.stop_timer)
 
     def stop_timer(self):
         self.running = False
+        self.scores.append((self.word_count, self.misshit_count))
         self.label.config(
             text=f"Words per Minute: {self.word_count}\nMisshit Count: {self.misshit_count}\nTime Left: 0")
+        self.update_score_table()
         self.start_button.config(state=tk.NORMAL)
         self.reset_button.config(state=tk.NORMAL)
 
-    def update_label(self):
+    def update_score_label(self):
         if self.running:
             self.label.config(
                 text=f"Words per Minute: {self.word_count}\nMisshit Count: {self.misshit_count}\nTime Left: {self.time_left}")
+            self.root.after(1000, self.update_score_label)
+
+    def update_time_label(self):
+        if self.running:
+            self.root.after(1000, self.update_time_label)
             self.time_left -= 1
-            self.root.after(1000, self.update_label)
 
     def reset_counter(self):
         self.word_count = 0
         self.misshit_count = 0
-        self.update_label()
+        self.update_score_label()
+
+    def update_score_table(self):
+        score_table_text = "Score Table:\n"
+        for idx, score in enumerate(self.scores, start=1):
+            score_table_text += f"{idx}. Words: {score[0]}, Misshits: {score[1]}\n"
+        self.score_table.config(text=score_table_text)
 
     def on_key_press(self, e):
         if self.running:
             if e.name == 'space' or e.name == 'enter':
                 self.word_count += 1
-                self.update_label()
+                self.update_score_label()
             elif e.name == 'backspace':
                 self.misshit_count += 1
-                self.update_label()
+                self.update_score_label()
 
     def monitor_key_presses(self):
         keyboard.on_press(self.on_key_press)
